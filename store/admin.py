700914887e7598d3aa991bb693a8aba0efcdc7a5
Arslan Rasheed <arslanrashee1@gmail.com>
2023-07-03 12:15:40 +0500

    admin collection product order added

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,11 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http.request import HttpRequest
 from . import models
+from django.db.models import Count
+from django.utils.html import format_html, urlencode
+from django.urls import reverse
 
 # * another way to register product model
 # class ProductAdmin(admin.ModelAdmin):
@@ -9,9 +15,14 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
@@ -28,5 +39,32 @@ class CustomerAdmin(admin.ModelAdmin):
     list_per_page = 10
 
 
-# Register your models here.
-admin.site.register(models.Collection)
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
+    list_select_related = ['customer']
+    list_per_page = 10
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'products_count']
+
+    # * rendering anchor link
+    @admin.display(ordering='products_count')
+    def products_count(self, collection):
+        # ? reverse('admin:app_model_page')
+        url = (reverse('admin:store_product_changelist')
+               + '?' +
+               urlencode({
+                   'collection__id': str(collection.id)
+               }))
+        return format_html('<a href="{}">{}</a>', url,
+                           collection.products_count)
+
+    # * return count
+    def get_queryset(self, request: HttpRequest) -> QuerySet[Any]:
+        return super().get_queryset(request).annotate(
+            products_count=Count('product')
+
+        )
--- store/models.py
+++ store/models.py
@@ -57,6 +57,12 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['first_name', 'last_name']
+
     # class Meta:
     #     db_table = 'store_customers'
     #     indexes = [
