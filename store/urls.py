a6be3fea8b769a541815adca1b764790d766f511
ArslanRasheed60 <arslanrashee1@gmail.com>
2023-07-18 14:22:38 +0500

    update routing

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter, DefaultRouter
+from pprint import pprint
 from . import views
 
+router = DefaultRouter()
+router.register('products', views.ProductViewSet)
+router.register('collections', views.CollectionViewSet)
+pprint(router.urls)
+
+
 # URL Config
-urlpatterns = [
-    path('products/', views.ProductList.as_view()),
-    path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('collections/', views.CollectionList.as_view()),
-    path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail')
-]
+
+urlpatterns = router.urls
+
+
+# urlpatterns = [
+        #path('', include(router.urls)),
+#     path('products/', views.ProductList.as_view()),
+#     path('products/<int:pk>/', views.ProductDetail.as_view()),
+#     path('collections/', views.CollectionList.as_view()),
+#     path('collections/<int:pk>/', views.CollectionDetail.as_view(), name='collection-detail')
+# ]
